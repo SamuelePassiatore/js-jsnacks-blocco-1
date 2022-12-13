d24996604f47c62f7cf657dfb1e30c5116db5802
SamuelePassiatore <semmisno2@protonmail.com>
2022-12-13 14:45:45 +0100

    prompt usernumber control

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -8,6 +8,7 @@ Il programma stampa la somma di tutti i numeri inseriti. */
 1 - Prendo l'elemento dal DOM;
 2 - Creo un ciclo for;
     2a - Chiedo all'utente di inserire un numero, tra 1 a 100;
+    2b - Controllo;
 3 - Aggiungo tutti i numeri ottenuti dall'utente a una variabile somma;
 4 - Stampo la somma finale;
 */
@@ -20,5 +21,13 @@ for (i = 0; i < 10; i++) {
     // 2a - Chiedo all'utente di inserire un numero, tra 1 e 100;
     const userNumber = parseInt(prompt('Inserisci un numero, tra 1 e 100', 30).trim());
     console.log(userNumber);
+    
+    // ! Controllo
+    if (isNaN(userNumber) || userNumber <= 0 || userNumber > 100) { 
+        alert('Devi inserire un numero tra 1 e 100');
+    }
+
+    
 }
+
 
