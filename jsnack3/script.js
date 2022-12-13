ac559e2e93efddf91a21feb45c2e9394b8fb233e
SamuelePassiatore <semmisno2@protonmail.com>
2022-12-14 00:37:35 +0100

    first commit - snack 3 bonus

---
+++ jsnack3/script.js
@@ -0,0 +1,16 @@
+console.log('JS OK');
+
+/* SNACK BONUS
+L'utente inserisce un numero di 4 cifre in un input. 
+Calcola la somma di tutte le cifre che compongono il numero 
+e stampala in pagina.*/
+
+/*
+1 - Prendo gli elementi dal DOM;
+2 - Aggancio l'event listener al button calcola;
+    3a - Recupero valore dell'input;
+    3b - Validazione degli input raccolti;
+    3c - Creo il ciclo for dei numeri raccolti;
+    3d - Svuoto il campo del numero;
+    3e - Stampo un messaggio sul risultato della somma;
+*/
