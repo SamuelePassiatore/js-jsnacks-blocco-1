1313c6a189cc2f6a6fe41c542f9eae83d5da333a
SamuelePassiatore <semmisno2@protonmail.com>
2022-12-13 15:10:46 +0100

    added general variables outside loop for & did sum

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -9,25 +9,30 @@ Il programma stampa la somma di tutti i numeri inseriti. */
 2 - Creo un ciclo for;
     2a - Chiedo all'utente di inserire un numero, tra 1 a 100;
     2b - Controllo;
-3 - Aggiungo tutti i numeri ottenuti dall'utente a una variabile somma;
+    2c - Aggiungo tutti i numeri ottenuti dall'utente a una variabile somma;
 4 - Stampo la somma finale;
 */
 
 // 1 - Prendo l'elemento dal DOM;
 const sum = document.getElementById('sum');
 
+// Variabili generiche
+let userNumber;
+let sumNumber = 0;
+
 // 2 - Creo un ciclo for;
 for (i = 0; i < 10; i++) {
     // 2a - Chiedo all'utente di inserire un numero, tra 1 e 100;
-    const userNumber = parseInt(prompt('Inserisci un numero, tra 1 e 100', 30).trim());
-    console.log(userNumber);
-    
-    // ! Controllo
+    userNumber = parseInt(prompt('Inserisci un numero, tra 1 e 100', 30).trim());
+    ////console.log(userNumber);
+    // 2b - Controllo
     if (isNaN(userNumber) || userNumber <= 0 || userNumber > 100) { 
         alert('Devi inserire un numero tra 1 e 100');
     }
-
-    
+    // 2c - Aggiungo tutti i numeri ottenuti dall'utente a una variabile somma;
+    sumNumber += userNumber;
 }
+
+console.log(sumNumber);
 
 
