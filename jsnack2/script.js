326648f63f334da6850afc126c8fdff2484b6bc2
SamuelePassiatore <semmisno2@protonmail.com>
2022-12-13 15:31:13 +0100

    first commit - snack2

---
+++ jsnack2/script.js
@@ -0,0 +1,19 @@
+console.log('JS OK');
+
+/*SNACK 2
+In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby. 
+L’utente inserisce il suo nome tramite un input.
+Comunicagli se può partecipare o no alla festa. */
+
+/*
+1 - Prendo gli elementi dal DOM;
+2 - Creo un array con cinque nomi autorizzati;
+3 - Aggancio l'event listener al button entrata;
+    3a - Recupero valore dell'input;
+    3b - Validazione degli input raccolti;
+    3c - Creo il ciclo for dei nomi autorizzati;
+    3d - Genero un condizionale if riguardo la possibile corrispondenza dei nomi;
+    3e - Genero un condizionale if sul messaggio da mostrare;
+    3f - Svuoto il campo del nome;
+    3g - Stampo un messaggio sull'esito del controllo;
+*/
