4bc212a9ab39fad6e62f7c6aa723678dbbcdfffe
SamuelePassiatore <semmisno2@protonmail.com>
2022-12-13 14:26:50 +0100

    first commit

---
+++ jsnack1/script.js
@@ -0,0 +1,14 @@
+console.log('JS OK');
+
+/*SNACK 1
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti. */
+
+/*
+1 - Prendo l'elemento dal DOM;
+2 - Chiedo all'utente di inserire un numero, tra 1 a 100;
+3 - Aggiungo tutti i numeri ottenuti dall'utente a una variabile somma;
+4 - Stampo la somma finale;
+*/
+
+
