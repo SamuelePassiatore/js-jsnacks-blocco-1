01a1b11913d1842da3b2707edac2945941d78d94
SamuelePassiatore <semmisno2@protonmail.com>
2022-12-14 01:33:42 +0100

    cicle for, empty number field and print sum on DOM

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -20,14 +20,32 @@ const inputNumber = document.getElementById('number');
 const button = document.getElementById('button');
 const result = document.getElementById('result');
 
+let sum = 0;
+
 // 2 - Aggancio l'event listener al button entrata;
 button.addEventListener('click', function () {
     // 2a - Recupero valore dell'input;
-    const userNumber = parseInt(inputNumber.value.trim());
+    const userNumber = inputNumber.value.trim();
     console.log(userNumber);
+    
     // 2b - Validazione degli input raccolti;
-    if(!userNumber || (isNaN(userNumber) || userNumber < 1000)){
+    if(!userNumber || (isNaN(userNumber) || userNumber < 1000 || userNumber > 9999)){
         alert('Devi inserire un numero di 4 cifre!');
         return;
     }
+
+    // 2c - Creo il ciclo for dei numeri raccolti;
+    for(let i = 0; i < userNumber.length; i++) {
+       sum += parseInt(userNumber[i]);
+    }
+    console.log(sum);
+
+    // 2d - Svuoto il campo del numero;
+    // const input number
+    inputNumber.value = '';
+
+    // 2e - Stampo un messaggio sul risultato della somma;
+    result.innerHTML = `La somma delle cifre che compongono il numero è: <strong>${sum}</strong>`;
 });
+
+
